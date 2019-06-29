f2ce1c3d772533961edf788b17a3c9a94fb2052f
tanisha2398 <tanishanegipro@gmail.com>
2019-06-29 16:08:58 +0530

    add webpack and using jquery library

---
+++ app.js
@@ -0,0 +1,8 @@
+let people = require("./people.js");
+let $ = require("jquery");
+// $("body").append("<h1>" + people[0].name + "</h1>");
+
+$.each(people, function(key, value) {
+  $("body").append("<h1>" + people[key].name + "</h1>");
+});
+console.log(people[0].name);
---
+++ people.js
@@ -0,0 +1,7 @@
+// function getHello() {
+//   return "Hello there";
+// }
+
+let people = [{ name: "Tanisha" }, { name: "manisha" }, { name: "anisha" }];
+
+module.exports = people;
